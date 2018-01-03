666ffd7537a984c9436125c9ea0d6789c7f20ed5
Elwin <elwindokter@gmail.com>
2018-01-03 17:17:42 +0100

    Aanpassingen aan "clone" button (https://3.basecamp.com/3135805/buckets/637988/todos/825896531). Clone field is nu een composer package (Github fork: https://github.com/reprovinci/kirby.field.duplicate)

--- template.php
+++ template.php
@@ -1,3 +1,44 @@
-<button type="button" class="btn btn-duplicate" id="<?php echo $field->id(); ?>" data-fieldname="<?php echo $field->name(); ?>"><i class="icon fa fa-clone"></i><?php e($buttontext, $buttontext, 'Clone page') ?></button>
-<input type="text" class="input input-duplicate" id="<?php echo $field->id(); ?>" name="<?php echo $field->name(); ?>" value="<?php echo $field->value(); ?>" placeholder="<?php e($placeholder, $placeholder, 'Enter page title and press Enter …') ?>">
+<?php // Trigger (show/hide toggle) button ?>
+<button
+  type="button"
+  class="btn btn-duplicate"
+  data-fieldname="<?= $field->name(); ?>"
+>
+  <i class="icon fa fa-clone"></i><?php e($buttontext, $buttontext, 'Clone page') ?>
+</button>
+
+<?php // Titel ?>
+<label class="label hide-duplicate"><?= l::get('pages.add.title.placeholder')?></label>
+<input
+  id="form-field-duplicate-title"
+  type="text"
+  class="input hide-duplicate"
+/>
+
+<?php // Url ?>
+<label class="label hide-duplicate"><?= l::get('pages.url.uid.label') ?></label>
+<input
+  id="form-field-duplicate-uid"
+  type="text"
+  class="input hide-duplicate"
+  value="<?= $page->uid()?>"
+/>
+
+<?php // Submit ?>
+<button
+  id="form-field-duplicate-submit"
+  class="btn btn-rounded btn-duplicate-submit hide-duplicate"
+>
+  <?php e($buttontextsubmit, $buttontextsubmit, 'Clone') ?>
+</button>
+
+<?php // Message to frontend ?>
 <div class="message-duplicate"></div>
+
+<?php // Redirect message ?>
+<input
+  id="form-field-duplicate-redirectmessage"
+  type="hidden"
+  value="<?= $redirectmessage ?:
+    'The page was successfully created. You will be redirected to the new page...'; ?>"
+/>
